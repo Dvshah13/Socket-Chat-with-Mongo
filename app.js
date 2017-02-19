7c9112191d98c5d43facb1f59b5689fb9ddf21a2
Dvshah13 <dvshah13@gmail.com>
2017-02-19 14:05:59 -0600

    Adding users

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ var express = require('express'),
     app = express(),
     server = require('http').createServer(app),
     io = require('socket.io').listen(server);
+    nicknames = [];
 
 server.listen(3000);
 
@@ -9,6 +10,18 @@ app.get('/', function(req, res){
     res.sendFile(__dirname + '/index.html');
 });
 
+io.socket.on('connection', function(socket){
+    socket.on('new user', function(data, callback){
+        if (nicknames.indexOf(data) != -1) {
+            callback(false);
+        }
+    });
+})
+
 io.sockets.on('connection', function(socket){
-    socket.on()
+    socket.on('send message', function(data){
+        io.sockets.emit('new message', data);
+        //to send to every user besides one who sent message use code below:
+        // socket.broadcast.emit('new message', data);
+    });
 });
